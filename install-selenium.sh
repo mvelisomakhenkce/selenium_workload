3adee43d92eb9d508d3aff48dd7fa1d8228039bc
mvelisomakhenkce <mvelisomakhenkce@gmail.com>
2024-08-03 00:56:29 +0200

    Update install-selenium.sh

--- install-selenium.sh
+++ install-selenium.sh
@@ -4,10 +4,10 @@ echo "Changing to home directory..."
 pushd "$HOME"
 
 echo "Update the repository and any packages..."
-sudo apt update && sudo apt upgrade -y
+apt update
 
 echo "Install prerequisite system packages..."
-sudo apt install wget curl unzip jq -y
+apt install wget curl unzip jq -y
 
 # Set metadata for Google Chrome repository...
 meta_data=$(curl 'https://googlechromelabs.github.io/chrome-for-testing/'\
@@ -18,7 +18,7 @@ echo "Download the latest Chrome binary..."
 wget $(echo "$meta_data" | jq -r '.channels.Stable.downloads.chrome[0].url')
 
 echo "Install Chrome dependencies..."
-sudo apt install ca-certificates fonts-liberation \
+apt install ca-certificates fonts-liberation \
     libappindicator3-1 libasound2 libatk-bridge2.0-0 libatk1.0-0 libc6 \
     libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgbm1 \
     libgcc1 libglib2.0-0 libgtk-3-0 libnspr4 libnss3 libpango-1.0-0 \
